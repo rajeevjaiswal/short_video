cd4c9be3a5321dec4b125086beb0403d0789598e
Rajeev Jaiswal <rajeevjaiswal26@gmail.com>
2020-08-02 18:15:07 +0530

    Added internet permission for release mode

--- lib/data/client/api_provider.dart
+++ lib/data/client/api_provider.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'package:shortvideo/data/model/video_model.dart';
 
 class ApiProvider {
-  String url = 'https://pinkvilla.com/';
+  String url = 'https://www.pinkvilla.com/';
 
   Future<List<VideoData>> getVideoFeed() async {
     http.Response res = await http.get('${url}feed/video-test/video-feed.json');
